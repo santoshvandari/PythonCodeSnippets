20efba32103f0f1e2a166fb34ea3dfba48c95440
Santosh Bhandari <santoshvandari1@gmail.com>
2025-08-21 18:05:22 +0545

    feat: google api usage snippet for location and place suggestion

--- GoogleAPIForPlaceSuggestion&LocationProvider/placesuggestion.py
+++ GoogleAPIForPlaceSuggestion&LocationProvider/placesuggestion.py
@@ -1,18 +1,17 @@
-
-import os,requests,asyncio
+import os, requests, asyncio
 
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+
+
 async def get_autocomplete_suggestions(query: str):
     try:
         google_places_url = "https://places.googleapis.com/v1/places:autocomplete"
         headers = {
-            'Content-Type': 'application/json',
-            'x-Goog-Api-Key': GOOGLE_API_KEY,
-            'X-Goog-FieldMask': 'suggestions.placePrediction.text.text'
+            "Content-Type": "application/json",
+            "x-Goog-Api-Key": GOOGLE_API_KEY,
+            "X-Goog-FieldMask": "suggestions.placePrediction.text.text",
         }
-        params = {
-            "input": query
-        }
+        params = {"input": query}
         response = requests.post(google_places_url, params=params, headers=headers)
         response.raise_for_status()
         data = response.json()
@@ -20,16 +19,24 @@ async def get_autocomplete_suggestions(query: str):
         if not suggested_places:
             return {"error": "No autocomplete suggestions found"}
 
-        suggestions = list(set(prediction.get("placePrediction").get("text").get("text") for prediction in suggested_places if prediction.get("placePrediction")))
+        suggestions = list(
+            set(
+                prediction.get("placePrediction").get("text").get("text")
+                for prediction in suggested_places
+                if prediction.get("placePrediction")
+            )
+        )
         return {"suggestions": suggestions}
 
     except Exception as err:
         return {"error": str(err)}
-    
+
+
 async def main():
     query = input("Enter your query for autocomplete suggestions: ")
     result = await get_autocomplete_suggestions(query)
     print(result)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
